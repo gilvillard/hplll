ae79366d26020f85c7b61d15909195b7ad807de3
Gilles <gilles.villard@ens-lyon.fr>
2017-09-23 15:56:27 +0200

    main draft

---
+++ drafts/main.cc
@@ -0,0 +1,162 @@
+
+#include <hplll.h>
+
+#include "relations.h"
+
+using namespace hplll;
+
+int main(int argc, char *argv[]) {
+
+	Timer time;
+
+	long alpha, d;
+
+	filebuf fb;
+	iostream os(&fb);
+
+	vector<FP_NR<mpfr_t> > fpv;
+
+	//  --------------   Test A
+
+	// static string s;
+
+	// fb.open ("C3_in", ios::in);
+
+	// os >> alpha;
+	// os >> d;
+
+	// mpfr_set_default_prec(alpha);
+
+	// fpv.resize(d);
+
+	// for (int i = 0; i < d; i++) {
+	// 	os >> s;
+	// 	mpfr_set_str (fpv[i].get_data(), s.c_str(), 10, GMP_RNDN);
+	// }
+
+	// fb.close();
+
+	// Z_mat<mpz_t> C;
+
+	// FPTuple<long, double> L(fpv);
+
+	// L.relation_f(C, alpha, 80, 20, 10, HLLL);
+
+	// cout << C << endl;
+
+
+	// ----------------  Test B
+
+
+	// alpha = 2800;
+	// mpfr_set_default_prec(alpha);
+
+	// d = 65;
+	// gen3r2s(fpv, d, 8, 8);
+
+
+
+	// ZZ_mat<mpz_t> C;
+
+	// FPTuple<long, double> L(fpv);
+
+	// L.relation_f(C, alpha, 60, 200, 20, HLLL);
+	// cout << C << endl;
+
+
+	// ----------------  Test C
+
+
+	// alpha = 1600;
+	// mpfr_set_default_prec(alpha);
+
+	// int r, s;
+	// r = 7;
+	// s = r;
+
+	// d = r*s+1;
+	// gen3r2s(fpv, d, r, s);
+
+
+
+	// ZZ_mat<mpz_t> C;
+
+
+
+	// cout << alpha << endl;
+	// cout << d << endl;
+	// for (int i = 0; i < d; i++) {
+	// 	//mpfr_out_str (stdout, 10, alpha, fpv[i].get_data(), GMP_RNDN);
+	// 	mpfr_printf ("%.1940Rf", fpv[i].get_data());
+	// 	cout << endl;
+	// }
+
+	// time.start();
+
+	// L.lll(C, alpha);
+
+	// //L.relation_f(C, alpha, 60, 100, 20, FPLLL);
+
+	// time.stop();
+
+	// cout << C << endl;
+
+	// cout << endl << endl << "   relation : " << time << endl ;
+
+	//  --------------   Test from file / Poisson
+
+	static string s;
+
+
+
+	//fb.open ("alpha.in", ios::in);
+
+	//os >> alpha;
+	//os >> d;
+
+	cin  >> alpha;
+	cin  >> d;
+
+	mpfr_set_default_prec(alpha);
+
+	fpv.resize(d);
+
+	for (int i = 0; i < d; i++) {
+		//os >> s;
+		cin >> s;
+		mpfr_set_str (fpv[i].get_data(), s.c_str(), 10, GMP_RNDN);
+	}
+
+	//fb.close();
+
+	ZZ_mat<mpz_t> C;
+
+
+	FPTuple<__int128_t, long double, matrix<FP_NR<long double> > > L(fpv);
+
+
+	//FPTuple<mpz_t, dpe_t, MatrixPE<double, dpe_t> > L(fpv);  // long double needs to comment long double in relation_z
+	//FPTuple<long, double, matrix<FP_NR<double> > > L(fpv);
+	//FPTuple<long, double,  > > L(fpv);
+
+
+	time.start();
+
+	L.relation_f(C, alpha, 30, 800, 20, FPLLL);
+	//L.relation_z(C, alpha, 20, 20, 40, FPLLL);    // -1 for bits only with mpz_t
+
+	time.stop();
+
+	cout << C << endl;
+
+	cout << endl << endl << "   relation : " << time << endl ;
+
+
+
+}
+
+
+
+
+
+
