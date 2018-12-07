38bc34cd97052127a3acdaf855690afdcdc90bc8
Gilles Villard <gilles.villard@ens-lyon.fr>
2018-12-07 18:34:41 +0100

    slll

--- drafts/main.cc
+++ drafts/main.cc
@@ -20,7 +20,6 @@ along with the hplll Library; see the file COPYING.LESSER.  If not, see
 http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
 
-
 #include "hplll.h"
 
 #include "wrappers.h"
